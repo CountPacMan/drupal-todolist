e765e52b1e7f561ea9b22040e9fd0c6c14538bc2
Daniel Toader & Cynthia Wilhelm <student@epicodus.com>
2015-05-13 11:21:44 -0700

    add init content

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -212,7 +212,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'tododb',
+      'username' => 'user',
+      'password' => 'password',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -244,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'kXQC1bF1r6zgdnJOVDw1NUR0clNxnkUpMlhOXnVvdO4';
 
 /**
  * Base URL (optional).
